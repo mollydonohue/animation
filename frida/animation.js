f475f57f2a18011a757a20c02d64e73001ec43fc
Molly <donoh95@uw.edu>
2017-09-24 16:35:26 -0700

    Added skeleton of ScrollMagic event

--- frida/animation.js
+++ frida/animation.js
@@ -4,10 +4,26 @@ $(document).ready(function() {
   TweenLite.to(document.getElementById("woman"), 2, {opacity:"1", delay:1});
   TweenLite.to(document.getElementById("knife"), 2, {opacity:"1", delay:2});
   TweenLite.to(document.getElementById("blood"), 2, {opacity:"1", delay:3});
-  TweenLite.to(document.getElementById("banner"), 2, {opacity:"1", delay:4});
 
   var imgheight = document.getElementById('img_area').clientHeight;
   var text = document.getElementById("text_area").setAttribute("style","height:" + imgheight + "px");
+
+
+  var controller = new ScrollMagic.Controller();
+
+  var bannerScene = new ScrollMagic.Scene({
+    triggerElement: '#banner_text'
+  })
+  .setClassToggle('#banner', 'fade-in') // add class to project01
+  .addIndicators({
+    name: 'fade scene',
+    colorTrigger: 'black',
+    indent: 200,
+    colorStart: '#75C695'
+  })
+  .addTo(controller);
+
+
 });
  
 
