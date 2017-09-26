a699c74e094a52410e1280b28449da515e8cef97
Molly <donoh95@uw.edu>
2017-09-25 21:22:28 -0700

    Updated Frida asset and text

--- frida/animation.js
+++ frida/animation.js
@@ -1,15 +1,9 @@
 $(document).ready(function() {
-  var man = document.getElementById("man");
-  TweenLite.to(man, 2, {opacity:"1"});
-  TweenLite.to(document.getElementById("woman"), 2, {opacity:"1", delay:1});
-  TweenLite.to(document.getElementById("knife"), 2, {opacity:"1", delay:2});
-  TweenLite.to(document.getElementById("blood"), 2, {opacity:"1", delay:3});
-
   var imgheight = document.getElementById('img_area').clientHeight;
   var text = document.getElementById("text_area").setAttribute("style","height:" + imgheight + "px");
-
+  var animate = ["woman", "man", "banner", "birds"];
+  
   var controller = new ScrollMagic.Controller();
-  // pin the intro
   var pinIntroScene = new ScrollMagic.Scene({
     triggerElement: '#text_area',
     triggerHook: 0,
@@ -17,19 +11,19 @@ $(document).ready(function() {
   .setPin('#intro', {pushFollowers: false})
   .addTo(controller);
 
-  var bannerScene = new ScrollMagic.Scene({
-    triggerElement: '#banner_text'
-  })
-  .setClassToggle('#banner', 'fade-in') // add class to project01
-  .addIndicators({
-    name: 'fade scene',
-    colorTrigger: 'black',
-    indent: 200,
-    colorStart: '#75C695'
-  })
-  .addTo(controller);
-
-
+  for (item in animate) {
+    var bannerScene = new ScrollMagic.Scene({
+      triggerElement: `#${animate[item]}_text`
+    })
+    .setClassToggle(`#${animate[item]}`, 'fade-in') // add class to project01
+    .addIndicators({
+      name: `${animate[item]} fade scene`,
+      colorTrigger: 'black',
+      indent: 200,
+      colorStart: '#75C695'
+    })
+    .addTo(controller);
+  }
 });
  
 
